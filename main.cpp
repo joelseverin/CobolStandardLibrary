3f112283e48489e8cc98267d259506f5c1191db6
Joel Severin <git@joelhemsida.se>
2017-04-24 22:00:57 +0200

    Removed old ALU class.

--- main.cpp
+++ main.cpp
@@ -6,31 +6,6 @@
 
 using namespace cobol;
 
-template<std::size_t L> class ALU {
-    typedef ByteArray<L> unpacked_t;
-    
-    public:
-        static unpacked_t add(unpacked_t lhs, unpacked_t rhs) {
-            unpacked_t result;
-            byte carry = 0;
-            for(std::size_t i = L - 1; i-- > 0; ) {// watch out for overflow
-                byte added = lhs[i] + rhs[i] + carry;
-                if(added > 10) {
-                    added = 0;
-                    carry = 1;
-                } else {
-                    carry = 0;
-                }
-
-                result[i] = added;
-            }
-            
-            if(carry) {
-                throw "Overflow Exception";
-            }
-        }
-};
-
 int main(int argc, char** argv) {
     FormattedNumber<3, 2> f(333, 33);
     f.print();
